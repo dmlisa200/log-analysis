6b88bd70717f38b9c4a628a7e6e28250422cb47a
dmlisa200 <17917202+dmlisa200@users.noreply.github.com>
2018-09-17 20:01:13 -0500

    python file for project

---
+++ newsdatadb.py
@@ -0,0 +1,98 @@
+
+import psycopg2
+
+
+def runs_query(query):
+   db = psycopg2.connect("dbname=news")
+   c = db.cursor()
+   c.execute(query)
+   answer = c.fetchall()
+   db.close()
+   return answer
+   
+   
+   
+def top_articles():
+    
+    query =  """
+        SELECT articles.title, COUNT(*) 
+        FROM articles
+        JOIN log
+        ON log.path LIKE CONCAT('/article/%', articles.slug)
+        GROUP BY articles.title
+        ORDER BY COUNT(*) desc
+        limit 3;
+    """
+    
+    results = runs_query(query)
+    
+    print('\nMOST POPULAR ARTICLES OF ALL TIME:')
+    count = 1
+    for i in results:
+        title = i[0]
+        views = '" has ' + str(i[1]) + " views"
+        print( title + views)
+        count+=1
+        
+        
+        
+def top_article_authors():
+    
+    query = """
+        SELECT authors.name, COUNT(*)
+        FROM authors
+        JOIN articles
+        ON authors.id = articles.author
+        JOIN log
+        ON log.path LIKE concat('/article/%', articles.slug)
+        GROUP BY authors.name
+        ORDER BY COUNT(*) DESC
+        LIMIT 3;
+    """
+    
+    results = runs_query(query)
+    
+    print('\nMOST POPULAR ARTICLE AUTHORS OF ALL TIME:')
+    count = 1
+    for i in results:
+        print( i[0] + ' has ' + str(i[1]) + " views")
+        count+=1
+        
+        
+        
+def days_with_errors():
+    
+    
+    query = """
+        SELECT total.day,
+           ROUND(((errors.error_requests*1.0) / total.requests), 3) AS percent
+        FROM (
+            SELECT date_trunc('day', time) "day", count(*) AS error_requests
+            FROM log
+            WHERE status = '404 NOT FOUND'
+            GROUP BY day) AS errors
+            JOIN (SELECT date_trunc('day', time) "day", count(*) AS requests
+            FROM log
+            GROUP BY day) AS total
+            ON total.day = errors.day
+            WHERE (ROUND(((errors.error_requests*1.0) / total.requests), 3) > 0.01)
+            ORDER BY percent DESC;
+        """    
+            
+    results = runs_query(query)
+        
+    print('\nDAYS WITH MORE THAN 1% OF REQUEST ERRORS:')
+    for i in results:
+        date = i[0].strftime('%B %d, %Y')
+        errors = str(round(i[1]*100, 1)) + "%" + " errors"
+        print(date + " has " + errors)
+            
+print('The Results Are:  ')
+top_articles()
+top_article_authors()
+days_with_errors()
+        
+        
+
+
+    
